64d4afa6d436a53829d5ca427b937d0d0090f4d1
MuhammadDandriHandian <55798941+MuhammadDandriHandian@users.noreply.github.com>
2019-11-08 00:17:21 +0700

    Add files via upload

---
+++ PBOIF2-10118062-Latihan60-Akatsuki/src/pboif2/pkg10118062/latihan60/akatsuki/kelompokakatsuki.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118062.latihan60.akatsuki;
+
+/**
+ *
+ * @author ASUS
+ */
+public class kelompokakatsuki {
+    public String nama;
+   
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void tampil(){
+       
+    }
+}
